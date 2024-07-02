db90ad7a186dd726736f30d82535e280f53e7da0
unknown <zarrin_r01@hotmail.co.uk>
2024-07-02 17:57:47 +0100

    added preliminary styling

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,9 +5,9 @@ const Navigation = () => {
         <>
             <nav>
                 <ul>
-                    <li><Link to='/create'>Make a graph</Link></li>
-                    <li><Link to='/how-it-works'>How it works</Link></li>
-                    <li><Link to='/about-us'>About us</Link></li>
+                    <li className="nav-item"><Link to='/create'>Make a graph</Link></li>
+                    <li className="nav-item"><Link to='/how-it-works'>How it works</Link></li>
+                    <li className="nav-item"><Link to='/about-us'>About us</Link></li>
                 </ul>
             </nav>
             <Outlet />
