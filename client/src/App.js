1e63a742dd28b9ea1b0d30451ebb5a3e9e75b5ec
unknown <zarrin_r01@hotmail.co.uk>
2024-07-02 17:09:17 +0100

    created router for different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,32 @@
-import { useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Graph from './components/Graph.js';
-import GraphInput from './components/GraphInput.js';
+import GraphContainer from './containers/GraphContainer.js';
+import About from './components/About.js';
+import Explanation from './components/Explanation.js';
 
 function App() {
 
-  const[group1Id, setGroup1Id] = useState(null);
-  const[group1Name, setGroup1Name] = useState("");
-  const[group2Id, setGroup2Id] = useState(null);
-  const[group2Name, setGroup2Name] = useState("");
-  const[question, setQuestion] = useState(null);
+  const wedgeRoutes = createBrowserRouter([
+    {
+        path: '/',
+        element: <GraphContainer />
+    },
+    {
+        path: '/about-us',
+        element: <About />
+    },
+    {
+        path: '/how-it-works',
+        element: <Explanation />
+    }
+  ]);
 
   return (
     <>
       <header className="header">
         <h1>Wedgesite</h1>
       </header>
-      <section>
-        <GraphInput
-          group1Id={group1Id}
-          setGroup1Id={setGroup1Id}
-          group1Name={group1Name}
-          setGroup1Name={setGroup1Name}
-          group2Id={group2Id}
-          setGroup2Id={setGroup2Id}
-          group2Name={group2Name}
-          setGroup2Name={setGroup2Name}
-          question={question}
-          setQuestion={setQuestion}/>
-        <Graph />
-      </section>
-      <section>
-        High level explanation of stuff here
-      </section>
-      <section>
-        Who we are here
-      </section>
+      <RouterProvider router={wedgeRoutes} />
     </>
   );
 }
