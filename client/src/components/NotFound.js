5576078063506ecf6c8c95ef29c919ea6a91f801
unknown <zarrin_r01@hotmail.co.uk>
2024-07-11 11:20:25 +0100

    404 page displays a short message

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -3,6 +3,7 @@ const NotFound = () => {
     return(
         <section>
             <h3>404: Page not found!</h3>
+            <p>What you're looking for doesn't seem to exist, sorry.</p>
         </section>
     );
 }
