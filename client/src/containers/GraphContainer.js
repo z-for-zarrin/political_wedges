e18989ee47c594a56b559d7a3857ab79106c89f9
unknown <zarrin_r01@hotmail.co.uk>
2024-07-14 17:21:59 +0100

    graph creation page gives user suggestions for parameters to try

--- client/src/containers/GraphContainer.js
+++ client/src/containers/GraphContainer.js
@@ -21,8 +21,17 @@ const GraphContainer = () => {
 
     return (
         <section className='page'>
-            <section id='parameter-explainer'>
-                Make a graph!
+            <section id='graph-creation-guide'>
+                <h3>Wondering where to start?</h3>
+                <p>For some interesting shapes, try the following:</p>
+                <ol id='suggestions'>
+                    <li>Green Party vs Reform Party leaners on 'For some crimes, the death penalty is the most appropriate sentence' (from 'Law and Order')</li>
+                    <i>Can you find an example with a higher polarisation value?</i>
+                    <li>High vs Low Income people on 'Governments should redistribute income...' (from 'Finance and Economics')</li>
+                    <i>How does the polarisation compare to what you expected? Now try some of the Law and Order questions for High vs Low Income.</i>
+                    <li>High vs Low Income on the issue of transgender people being allowed to change their birth certificates (from 'Gender and Sexual Minorities')</li>
+                    <i>Why do you think the 'jump' in the middle occurs?</i>
+                </ol>
             </section>
             <GraphInput
                 group1Id={group1Id}
