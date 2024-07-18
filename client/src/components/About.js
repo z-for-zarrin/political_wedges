edcac413fe47021683728198d9bd2e4f5caf446c
unknown <zarrin_r01@hotmail.co.uk>
2024-07-18 15:07:25 +0100

    about page shows profiles for both Joey and Zarrin

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,18 +3,34 @@ const About = () => {
         <section className="page">
             <section className="profile">
                 <h3>Joey Cartwright — Backend Developer & Data Scientist</h3>
-                <p>Description under construction, but trust me he's cool</p>
+                <p>Joey, like his co-creator and (according to historians) close friend Zarrin,
+                    spent his post-secondary years at the University of Warwick. There, he
+                    co-founded and was eventually president of the Bubble Tea Society, and studied
+                    for a degree in Economics and Politics in his spare time. He developed the
+                    polarisation parallelogram during his panic master's in quantitative politics,
+                    and began working on the python code for what would eventually become the graph
+                    generator you see here not long after. His first job was writing practice
+                    questions for an introductory textbook in Quantitative Social Science under
+                    a professor who accused him of having 'chicken issues'. He is always up for
+                    a detailed conversation on the best places to find chicken wings (and once
+                    created a chicken wing tier list data set from his friend group after a
+                    'Chicken Crawl'), but will happy also talk to you about 3D <i>Zelda</i> or
+                    <i>Mario</i> titles, murder mysteries, or 20<sup>th</sup> century jazz pianists.</p>
             </section>
             <section className="profile">
                 <h3>Zarrin Rahman — Frontend Developer</h3>
-                <p>Hailing from the town of Bedford, Bedfordshire, with a Mathematics
-                    degree from the University of Warwick, Warwickshire, Zarrin has turned
-                    her eyes to tech for the next step in her journey. Since graduating,
+                <p>Hailing from the town of Bedford, Bedfordshire, Zarrin earned her Mathematics
+                    degree from the University of Warwick, Warwickshire, where she also met Joey
+                    through the Bubble Tea Society. Eventually she became the publicity officer
+                    for the society, which should give her sufficient credentials to claim graphic
+                    design is her passion (just look at the lovely logo she designed). Now, Zarrin
+                    has turned her eyes to tech for the next step in her journey. Since graduating,
                     she has completed a bootcamp run by the Bright Network Technology Academy,
                     where she learnt about full-stack software development — including the
                     knowledge necessary for building this very project! When she's not tapping
                     away at the laptop, you can find Zarrin playing guitar, getting lost in
-                    the woods or farming her time away in Stardew Valley.</p>
+                    the woods or farming her time away in Stardew Valley. Please direct all webdev
+                    queries and cat pictures to her.</p>
             </section>
         </section>
     );
