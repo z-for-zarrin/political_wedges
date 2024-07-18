c289c19281277ccbd5a9e21539a7da49df3db276
unknown <zarrin_r01@hotmail.co.uk>
2024-07-18 18:23:33 +0100

    How it works page links to full academic explanation pdf

--- client/src/components/Explanation.js
+++ client/src/components/Explanation.js
@@ -1,7 +1,10 @@
+import explanationPDF from '../assets/Parallelogram-Description-And-Proofs.pdf';
+
 const Explanation = () => {
     return(
         <section className="page">
-            About the measure
+            <i>For a more academic and mathematical explanation, including various semi-formal proofs,
+            you can download <a href={explanationPDF}>this pdf</a>. For those of you with social skills, you may find the below a bit more helpful instead</i>
         </section>
     );
 }
