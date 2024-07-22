5d0870f363e04054ceb1974229eada9bf7bf86c7
unknown <zarrin_r01@hotmail.co.uk>
2024-07-22 19:09:48 +0100

    text is centred

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,7 +1,9 @@
+import '../styles/404Style.css'
+
 const NotFound = () => {
     
     return(
-        <section>
+        <section id="not-found">
             <h3>404: Page not found!</h3>
             <p>What you're looking for doesn't seem to exist, sorry.</p>
         </section>
